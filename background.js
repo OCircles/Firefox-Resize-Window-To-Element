979e14231d1610b6bbae2de818d0c86f6b6d9997
OCircles <o.qpost@gmail.com>
2025-03-30 23:21:32 +0200

    Made startable from toolbar

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
+// Create context menu item "Resize To Element" that starts the picker
 browser.contextMenus.create({
-  id: "element-picker",
-  title: "Resize To Element",
+  id: "resize-to-element",
+  title: "Resize Window To Element",
   contexts: ["all"]
 });
 
@@ -31,28 +32,34 @@ function getAdjustedSize(width, height) {
   });
 }
 
+// Handle context menu click to start picker
 browser.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "resize-window") {
-    browser.tabs.sendMessage(tab.id, { action: "getElementSize" }).then((response) => {
-      if (response && response.width && response.height) {
-        getAdjustedSize(response.width, response.height).then((size) => {
-          resizeWindow(size.width, size.height, tab.id);
-        });
-      } else {
-        console.error("Could not get element size");
-      }
-    }).catch((error) => {
-      console.error("Error getting element size:", error);
-    });
-  } else if (info.menuItemId === "element-picker") {
+  if (info.menuItemId === "resize-to-element") {
     browser.tabs.sendMessage(tab.id, { action: "startPicker" });
   }
 });
 
+// Handle resize message from content script
 browser.runtime.onMessage.addListener((message, sender) => {
   if (message.action === "triggerResize" && message.width && message.height) {
     getAdjustedSize(message.width, message.height).then((size) => {
       resizeWindow(size.width, size.height, sender.tab.id);
     });
+  }
+});
+
+// Handle browser action click to start picker
+browser.browserAction.onClicked.addListener((tab) => {
+  browser.tabs.sendMessage(tab.id, { action: "startPicker" });
+});
+
+// Ensure the browser action is enabled for all tabs
+browser.tabs.onActivated.addListener((activeInfo) => {
+  browser.browserAction.enable(activeInfo.tabId);
+});
+
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete") {
+    browser.browserAction.enable(tabId);
   }
 });
